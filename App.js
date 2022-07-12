28a6ca3914d9f773ec9c779f3dcf24b8319989d8
vprahul2008 <73024199+vprahul2008@users.noreply.github.com>
2022-07-12 15:48:37 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DrawerNavigator from "./navigation/DrawerNavigator";
 
 export default function App() {
   return (
